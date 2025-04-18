097e150b3091824560d11e440a36164b2d373fe3
Bhunesh ojha108 <149822963+Bhuneshojha@users.noreply.github.com>
2025-04-18 19:31:32 +0500

    dataStore.py

--- dataStore.py
+++ dataStore.py
@@ -5,7 +5,7 @@ import hashlib
 import json
 import os
 import time
-from cryptography.fernet import fernet 
+from cryptography.fernet import Fernet 
 from base64 import urlsafe_b64decode
 from hashlib import pbkdf2_hmac
 
